be2290a75c86e00d3e90c3a7fb31b16596c95db1
huutai <huutt201@example.com>
2026-05-03 17:41:30 +0700

    first commit

---
+++ app/build/generated/source/buildConfig/debug/huutai/dev/meetmino/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package huutai.dev.meetmino;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "huutai.dev.meetmino";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String BASE_URL = "http://192.168.1.3:3000/";
+  // Field from build type: debug
+  public static final Boolean ENABLE_CRASH_REPORTING = false;
+  // Field from build type: debug
+  public static final Boolean ENABLE_LOGGING = true;
+}
